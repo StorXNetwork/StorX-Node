453cd4adc03667aa40cd30b18d1a6fdc84c05e39
Debchakra Majumder <dmajumder4292@gmail.com>
2021-07-27 18:14:13 +0530

    Add success message in upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,3 +6,4 @@ echo "Upgrading Docker Images"
 sudo docker pull storxnetwork/storxnode:latest
 sudo docker-compose -f docker-services.yml down
 sudo docker-compose -f docker-services.yml up -d
+echo "Congrats! Your Node has been successfully updated with latest changes!"
