81661a2757ff6fd16ac4482569e7fb26fa6d9725
Kasim Hussain <kasiimh1@gmail.com>
2021-12-10 19:20:00 +0000

    add the ability to update the distro per StorX VPS guidance when upgrading node

--- upgrade.sh
+++ upgrade.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+echo "Fetching Distro Updates & Upgrades"
+apt update 
+apt upgrade -y 
+apt dist-upgrade -y 
 echo "Upgrading StorX Network Configuration Scripts"
 git pull
 echo "Upgrading Docker Images"
@@ -7,3 +10,5 @@ sudo docker pull storxnetwork/storxnode:latest
 sudo docker-compose -f docker-services.yml down
 sudo docker-compose -f docker-services.yml up -d
 echo "Congrats! Your Node has been successfully updated with latest changes!"
+echo "Restarting VPS ....."
+sudo reboot
